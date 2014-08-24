aa203fb783befc296e983f6e8f98a5a0fa29dad9
Peter DeMartini <peterdemartini@me.com>
2014-08-23 22:06:20 -0700

    Build Files

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Peter DeMartini
 ================
 
 I hack my way through life, without sacrificing creativity and passion. I specialize in web applications; the tools of my trades are JavaScript, CodeIgniter and the #IoT.
+
+
+Deploy:
+
+npm run deploy
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Metalsmith  = require('metalsmith'),
+var Metalsmith = require('metalsmith'),
     markdown = require('metalsmith-markdown'),
     templates = require('metalsmith-templates'),
     collections = require('metalsmith-collections'),
@@ -9,7 +9,7 @@ var Metalsmith  = require('metalsmith'),
     fs = require('fs');
 
 
-function getPartial(name){
+function getPartial(name) {
     return fs.readFileSync(__dirname + '/templates/partials/' + name + '.hbt').toString();
 }
 
@@ -18,20 +18,7 @@ Handlebars
 Handlebars
     .registerPartial('footer', getPartial('footer'));
 
-function findTemplate(config) {
-    var rgx = new RegExp(config.pattern);
-
-    return function(files, metalsmith, done) {
-        for (var file in files) {
-            if (rgx.test(file)) {
-                if (!files[file].template) {
-                    files[file].template = config.templateName;
-                }
-            }
-        }
-        done();
-    };
-}
+var rootFiles = {};
 
 Metalsmith(__dirname)
     .use(collections({
@@ -49,11 +36,48 @@ Metalsmith(__dirname)
         templateName: 'post.hbt'
     }))
     .use(markdown())
+    .use(getRoot())
     .use(permalinks({
         pattern: ':collection/:title'
     }))
+    .use(reAddFiles())
     .use(templates('handlebars'))
     .destination('./build')
     .build(function(err, files) {
         if (err) { throw err; }
     });
+
+function getRoot(config){
+    return function(files, metalsmith, done){
+        for (var file in files) {
+            if(!~file.indexOf('/')){
+                rootFiles[file] = files[file];
+                delete files[file];
+            }
+        }
+        done();
+    };
+}
+
+function reAddFiles(){
+    return function(files, metalsmith, done){
+        for (var file in rootFiles) {
+            files[file] = rootFiles[file];
+            delete rootFiles[file];
+        }
+        done();
+    };
+}
+
+function findTemplate(config) {
+    var rgx = new RegExp(config.pattern);
+
+    return function(files, metalsmith, done) {
+        for (var file in files) {
+            if (rgx.test(file) && !files[file].template) {
+                files[file].template = config.templateName;
+            }
+        }
+        done();
+    };
+}
